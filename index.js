f2bf6f3cefc74d331bb6b7bdc510b0380cee0a8a
Andre <andre-dias-1@hotmail.com>
2023-05-17 15:47:50 -0300

    core: create store and dispatch actions

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-//action: an object with a type property
+const redux = require('redux')
+const createStore = redux.createStore; // to create the store through the method createStore
 
+//action: an object with a type property
 //action creator: a function that returns an object
 const PRODUCT_ORDERED = "PRODUCT_ORDERED";
 
@@ -24,8 +26,26 @@ const reducer = (state = initialState, action ) => {
         case PRODUCT_ORDERED:
             return  {
                 ...state,
-                numOfProducts = state.numOfProducts -1
+                numOfProducts: state.numOfProducts -1
             }
     }
     
 }
+
+//the redux store holding the application state (1)
+const store = createStore(reducer)
+
+// Allow access to state via getState() (2)
+console.log('Initial state:', store.getState());
+
+// Register listeners via subscribe() (3)
+//store.subscribe is going to return a function
+const unsubscribe = store.subscribe(() => console.log("Updated state:" , store.getState()))
+
+//Provide the dispatch method to update the state
+store.dispatch(orderProduct())
+store.dispatch(orderProduct())
+store.dispatch(orderProduct())
+
+//Unregister of listeners via the functin returned by subscribe() (4)
+unsubscribe()
